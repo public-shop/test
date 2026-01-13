216908898bcb8ed052865815cfbf87db1978fec7
public-shop <kongespresso3@gmail.com>
2026-01-13 12:10:37 +0700

    Update test.php

--- test.php
+++ test.php
@@ -24,7 +24,7 @@
   "title_en": "Animal Coloring Book",
   "category_th": "ระบายสี",
   "category_en": "Coloring",
-  "cover": "https://xn--q3caqql0avca2fsa7ntb1d.com/wp-content/uploads/2023/09/Screenshot-6406.png",
+  "cover": "https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.6435-9/186330509_272438664675250_8862102970351742486_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=rlpTZAKEOawQ7kNvwEvprJd&_nc_oc=AdlrXCMiSY5_KHFoJ_oJwWD4NUHwh2VVbY6MJFX_c9qVeuOSjNQgId7o4V53XDL4OxM&_nc_zt=23&_nc_ht=scontent.fbkk2-8.fna&_nc_gid=Y9rRzHLGNopJUELynVYhaw&oh=00_AfrTkP4gHZjJOxaHSAGISbDCphXB5qjau_rzv4QxyK1VRA&oe=698D56EC",
   "pdf": "https://zerosocket.xyz/zero/test.pdf",
   "cost": 30,
   "preview": [
@@ -37,7 +37,7 @@
   "title_en": "Animal Coloring Book",
   "category_th": "ระบายสี",
   "category_en": "Coloring",
-  "cover": "https://xn--q3caqql0avca2fsa7ntb1d.com/wp-content/uploads/2023/09/Screenshot-6406.png",
+  "cover": "https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.6435-9/187136255_272438718008578_5040720845101361395_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_ohc=dq56UqwMucIQ7kNvwHrMGim&_nc_oc=AdlRnCALs3sLduzMM-F2ph9NjlW4D3UbBPFar1uSO7EzPPiH686D3-UhWCGD8DIl-Go&_nc_zt=23&_nc_ht=scontent.fbkk2-8.fna&_nc_gid=kNFZemwtT8TdkhJirSzdsA&oh=00_AfpW6xE66ZZd7SDAbnuIkrLQBuz28dSOg4yjtjSwpCFZ7w&oe=698D3175",
   "pdf": "https://zerosocket.xyz/zero/test.pdf",
   "cost": 30,
   "preview": [
