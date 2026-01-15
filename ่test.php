aa709eace7e2d6dbf35ff7d5b5f5980bb2a7b60d
public-shop <kongespresso3@gmail.com>
2026-01-15 12:04:38 +0700

    Update ID: GeWBCqm6y1

---
+++ ่test.php
@@ -0,0 +1,25 @@
+[
+  {
+    "id": "GeWBCqm6y1",
+    "title_th": "กดแห",
+    "title_en": "dfgdfg",
+    "category_th": "dfgfdg",
+    "category_en": "dfgdf",
+    "cover": "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/cover.jpg",
+    "pdf": "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/doc.pdf",
+    "cost": 30,
+    "preview": [
+      "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/preview_1.jpg",
+      "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/preview_2.jpg",
+      "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/preview_3.jpg",
+      "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/preview_4.jpg",
+      "https://raw.githubusercontent.com/public-shop/test/main/storage/GeWBCqm6y1/preview_5.jpg"
+    ],
+    "tags_th": [
+      "fdgfd"
+    ],
+    "tags_en": [
+      "dgfdfg"
+    ]
+  }
+]
